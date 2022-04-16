eb60ce4b12f6609dcc679c9905c9b004d41437f1
natalie <nataliemae263@gmail.com>
2022-04-16 11:05:35 -0500

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'/public/PalmTreeCaliforniaLocationHouse.JPG'} className="Palm Tree" alt="Palm Tree in the Sun" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
